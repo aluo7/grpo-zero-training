d28b3b3a92c1fe1e74e692557a2c8d49bab8b51a
Alan Luo <luoalan07@gmail.com>
2025-10-01 20:53:41 -0500

    update prompt logic

--- test/test_inference.py
+++ test/test_inference.py
@@ -4,7 +4,7 @@ from datasets import load_dataset
 
 torch.random.manual_seed(0)
 
-# --- load model and tokenizer ---
+# load model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(
     "microsoft/Phi-3-mini-128k-instruct",
     device_map="cuda",
@@ -14,19 +14,19 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
 
-# --- load dataset ---
+# load dataset
 dataset = load_dataset("qwedsacf/competition_math")
 
-example_problem = dataset["train"][5]["problem"]  # test on a single input
-prompt = [
-    {"role": "user", "content": f"Solve the following math problem, providing a step-by-step chain of thought. Enclose the final answer in \\boxed{{}}.\n\nProblem:\n{example_problem}"},
-    {"role": "assistant", "content": "Solution:\n"},
+example = dataset["train"][5]["problem"]  # test on a single input
+messages = [
+    {"role": "user", "content": f"Solve the following math problem, providing a step-by-step chain of thought. Enclose the final answer in \\boxed{{}}.\n\nProblem:\n{example}"},
+    {"role": "assistant", "content": "Solution:\n"}
 ]
 
-prompt_string = tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True, return_tensors="pt")
+prompt_string = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, return_tensors="pt")
 tokenized_prompt = tokenizer(prompt_string, return_tensors="pt").to(model.device)
 
-# --- run inference ---
+# run inference
 generation_args = { 
     "max_new_tokens": 512,
     "do_sample": False,  # force greedy decoding - good for math
@@ -34,10 +34,10 @@ generation_args = {
 
 outputs = model.generate(**tokenized_prompt, **generation_args)
 
-# --- decode and output result ---
+# decode and output result
 output_text = tokenizer.decode(outputs[0][tokenized_prompt['input_ids'].shape[1]:], skip_special_tokens=True)
 
 print("--- PROBLEM ---")
-print(example_problem)
+print(example)
 print("\n--- MODEL OUTPUT ---")
 print(output_text)
